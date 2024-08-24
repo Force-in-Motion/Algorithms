1f947847b9ae8de92977b8d6b3bc89a5802910dc
Sergey Gvozdenko <canzdao@gmail.com>
2024-08-24 22:10:37 +0300

    fix

---
+++ Алгоритмическая сложность методов разных структур данных.py.py
@@ -0,0 +1,37 @@
+# stack
+# push(itm)|O(1)/O(1)
+# pop()|O(1)/O(1)
+# peek()|O(1)/O(1)
+# is_empty()|O(1)/O(1)
+
+
+# linked_list
+# add(item)| O(1)/O(n)/O(n)
+# add_front(item)|O(1)/O(1)/O(1)
+# remove(item)|O(1)/O(n)/O(n)
+# insert(item,index)|O(1)/O(n)/O(n)
+# is_empty()|O(1)/O(1)/O(1)
+# find(item)|O(1)/O(n)/O(n)
+# count(item)|O(1)/O(n)/O(n)
+# add_list(collection)|O(1)/O(n)/O(n)
+
+
+# queue
+# enqueue(itm)||O(1)/O(1)
+# dequeue()||O(1)/O(1)
+# peek()||O(1)/O(1)
+# is_empty()||O(1)/O(1)
+
+
+# dynamic_array
+# [] | O(1)/O(1)/O(1)
+# =  | ---------------
+# add_front(item)| O(1)/O(n)/O(n)
+# add_list(collection)| O(1)/O(n)/O(n)
+# add(item)|O(1)/O(1)/O(n)
+# insert(item,index)| O(1)/O(n)/O(n)
+# remove(item)| O(1)/O(n)/O(n)
+# remove(item)| O(1)/O(n)/O(n)
+# is_empty()|O(1)/O(1)/O(1)
+# count(item)| O(1)/O(n)/O(n)
+# find(item)| O(1)/O(n)/O(n)
